c8886509370df422544af4c4fb73b0ca4e7c5c14
kobydamari <koby.damari@playbuzz.com>
2018-07-07 11:27:03 +0300

    pandaio

--- express.js
+++ express.js
@@ -1,11 +1,28 @@
-const express    = require('express');
-const app        = express();
-const portNumber = 3000;
-const sourceDir  = 'dist';
+const express = require('express');
+var cors = require('cors')
+const app = express();
+const bodyParser = require('body-parser');
+const sourceDir = 'dist';
+const mongoose = require('mongoose');
+
+const port = process.env.PORT || 8082;
 
 app.use(express.static(sourceDir));
+app.use(cors());
 
-app.listen(portNumber, () => {
-  console.log(`Express web server started: http://localhost:${portNumber}`);
-  console.log(`Serving content from /${sourceDir}/`);
+// Body Parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use('/api', require('./src/server/routes/comments'));
+
+const uri = 'mongodb://koby:1234@bigpandaownz-shard-00-00-sdkkk.mongodb.net:27017,bigpandaownz-shard-00-01-sdkkk.mongodb.net:27017,bigpandaownz-shard-00-02-sdkkk.mongodb.net:27017/test?ssl=true&replicaSet=BigPandaOwnz-shard-0&authSource=admin&retryWrites=true';
+mongoose.connect(uri).then(
+    () => console.log('connected'),
+    err => console.log('not connected', err)
+);
+
+app.listen(port, () => {
+    console.log(`Express web server started: http://localhost:${port}`);
+    console.log(`Serving content from /${sourceDir}/`);
 });
